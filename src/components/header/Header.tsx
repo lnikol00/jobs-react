093e7add390ff6ec55b6f61aaceb6166558abf14
Koce1 <nikolac007@gmail.com>
2023-04-16 20:06:11 +0200

    correction

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,15 +1,31 @@
 import styles from "./header.module.css"
 import * as SiIcons from "react-icons/si"
+import * as FaIcons from "react-icons/fa"
+import * as AiIcons from "react-icons/ai"
 import { Link } from "react-router-dom"
+import { useState } from "react"
 
 function Header() {
+
+    const [open, setOpen] = useState<boolean>(false)
+
+    const onChange = () => {
+        setOpen(!open)
+    }
+
     return (
         <div className={styles.headerContainer}>
+            <div className={styles.menuItem}>
+                <FaIcons.FaBars onClick={onChange} />
+            </div>
             <Link to="/" className={styles.heading}>
                 <SiIcons.SiMicrosoftoffice />
                 <h2>Job Center</h2>
             </Link>
-            <div className={styles.links}>
+            <div className={open ? `${styles.closed} ${styles.links}` : `${styles.links}`} onClick={onChange}>
+                <li className={styles.close}>
+                    <AiIcons.AiOutlineClose onClick={onChange} />
+                </li>
                 <li>
                     <Link to="find" className={styles.findLink}>Find a Job</Link>
                 </li>
