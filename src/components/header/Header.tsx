2116efd74b4d62d853ebec36957d248990c815aa
Koce1 <nikolac007@gmail.com>
2023-04-19 21:25:18 +0200

    final touch

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,40 +1,44 @@
 import styles from "./header.module.css"
 import * as SiIcons from "react-icons/si"
-import * as FaIcons from "react-icons/fa"
-import * as AiIcons from "react-icons/ai"
 import { Link } from "react-router-dom"
 import { useState } from "react"
 
 function Header() {
 
     const [open, setOpen] = useState<boolean>(false)
+    const [bars_class, setBarsClass] = useState(`${styles.menuBars} ${styles.menuUnclicked}`)
 
     const onChange = () => {
+        if (!open) {
+            setBarsClass(`${styles.menuBars} ${styles.menuClicked}`)
+        }
+        else {
+            setBarsClass(`${styles.menuBars} ${styles.menuUnclicked}`)
+        }
         setOpen(!open)
     }
 
     return (
-        <div className={styles.headerContainer}>
-            <Link to="/" className={styles.heading}>
-                <SiIcons.SiMicrosoftoffice />
-                <h2>Job Center</h2>
-            </Link>
-            <div className={open ? `${styles.links}` : ` ${styles.active} ${styles.links}`}>
-                <li>
-                    <Link to="find" className={styles.findLink} onClick={onChange}>Find a Job</Link>
-                </li>
-                <li>
-                    <Link to="post" className={styles.postLink} onClick={onChange}>Post a Job</Link>
-                </li>
-            </div>
-            {
-                !open ? <div className={styles.menuItem}>
-                    <FaIcons.FaBars onClick={onChange} />
-                </div> :
-                    <div className={styles.close}>
-                        <AiIcons.AiOutlineClose onClick={onChange} />
-                    </div>
-            }
+        <div className={styles.mainContainer}>
+            <nav className={styles.headerContainer}>
+                <div className={styles.barsContainer} onClick={onChange}>
+                    <div className={bars_class}></div>
+                    <div className={bars_class}></div>
+                    <div className={bars_class}></div>
+                </div>
+                <Link to="/" className={styles.heading}>
+                    <SiIcons.SiMicrosoftoffice />
+                    <h2>Job Center</h2>
+                </Link>
+                <div className={open ? `${styles.links}` : ` ${styles.active} ${styles.links}`}>
+                    <li>
+                        <Link to="find" className={styles.findLink} onClick={onChange}>Find a Job</Link>
+                    </li>
+                    <li>
+                        <Link to="post" className={styles.postLink} onClick={onChange}>Post a Job</Link>
+                    </li>
+                </div>
+            </nav>
         </div>
     )
 }
